572203e5da48ed853aa0ffc0697df0bd70c647bd
Xavier Dias <93143666+Xavi007@users.noreply.github.com>
2023-02-06 23:50:53 +0530

    Update Treasure_Island.py

--- Treasure Island/Treasure_Island.py
+++ Treasure Island/Treasure_Island.py
@@ -35,7 +35,7 @@ if choice1 == "left":
     elif choice3 == "yellow":
       print("You found the treasure! You Win!")
     elif choice3 == "blue":
-      print("You enter a room of beasts. Game Over.")
+      print("You enter a room of beasts. Game Over")
     else:
       print("You chose a door that doesn't exist. Game Over")
   else:
